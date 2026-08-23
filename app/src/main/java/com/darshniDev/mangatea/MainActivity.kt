67a195e03b2bf3a3f8a0c94651998b15a1e9718a
Divya-Darshan <parukrish.darshan20@gmail.com>
2026-08-23 08:47:08 +0530

    testing .xml & .kt

--- app/src/main/java/com/darshniDev/mangatea/MainActivity.kt
+++ app/src/main/java/com/darshniDev/mangatea/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.darshniDev.mangatea
 
+import com.darshniDev.mangatea.R
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Using safe call operator ?. so it will NEVER crash if null
+        val myTextView = findViewById<TextView>(R.id.myTextView)
+        myTextView?.text = "Vanakkam! MangaTea is Ready 🍵"
     }
 }
